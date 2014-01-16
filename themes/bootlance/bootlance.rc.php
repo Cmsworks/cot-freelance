227e48a2f7a0be8a806a4eb6ade10b66cd73b1c3
devkont <yusupovb@gmail.com>
2014-01-16 11:49:30 +0400

    fixed #16

--- themes/bootlance/bootlance.rc.php
+++ themes/bootlance/bootlance.rc.php
@@ -11,15 +11,14 @@
 
 defined('COT_CODE') or die('Wrong URL.');
 
-cot_rc_add_file($cfg['themes_dir'].'/'.$usr['theme'].'/css/reset.css');
-cot_rc_add_file($cfg['themes_dir'].'/'.$usr['theme'].'/css/modalbox.css');
-cot_rc_add_file($cfg['themes_dir'].'/'.$usr['theme'].'/css/style.css');
-cot_rc_add_file($cfg['themes_dir'].'/'.$usr['theme'].'/js/js.js');
 
 cot_rc_add_file($cfg['themes_dir'].'/'.$usr['theme'].'/bootstrap/css/bootstrap.min.css');
 cot_rc_add_file($cfg['themes_dir'].'/'.$usr['theme'].'/bootstrap/css/bootstrap-responsive.css');
 
-//cot_rc_link_footer($cfg['themes_dir'].'/'.$usr['theme'].'/bootstrap/js/bootstrap.min.js');
+cot_rc_add_file($cfg['themes_dir'].'/'.$usr['theme'].'/css/modalbox.css');
+cot_rc_add_file($cfg['themes_dir'].'/'.$usr['theme'].'/css/style.css');
+
+cot_rc_add_file($cfg['themes_dir'].'/'.$usr['theme'].'/js/js.js');
 
 require_once cot_rc($cfg['themes_dir'].'/'.$usr['theme'].'/'.$usr['theme'].'.resources.php');
 
