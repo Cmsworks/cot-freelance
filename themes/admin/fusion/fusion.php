2057bb491c22f72b6b9feaa8dd51a485ca09cda9
devkont <yusupovb@gmail.com>
2014-01-21 20:42:22 +0400

    Подключаю недостающих ресурсов для админ-темы

---
+++ themes/admin/fusion/fusion.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('COT_CODE') or die('Wrong URL');
+
+$R['code_title_page_num'] = ' (' . $L['Page'] . ' {$num})';
+$R['link_pagenav_current'] = '<li class="active"><a href="{$url}"{$event}{$rel}>{$num}</a></li>';
+$R['link_pagenav_first'] = '<li><a href="{$url}"{$event}{$rel}>'.$L['pagenav_first'].'</a></li>';
+$R['link_pagenav_gap'] = '<li>...</li>';
+$R['link_pagenav_last'] = '<li><a href="{$url}"{$event}{$rel}>'.$L['pagenav_last'].'</a></li>';
+$R['link_pagenav_main'] = '<li><a href="{$url}"{$event}{$rel}>{$num}</a></li>';
+$R['link_pagenav_next'] = '<li><a href="{$url}"{$event}{$rel}>'.$L['pagenav_next'].'</a></li>';
+$R['link_pagenav_prev'] = '<li><a href="{$url}"{$event}{$rel}>'.$L['pagenav_prev'].'</a></li>';
+
+
+?>
