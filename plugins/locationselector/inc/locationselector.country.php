af005418b814aa23f4b4896dbcc0089cb8ae9c97
devkont <yusupovb@gmail.com>
2014-01-31 13:48:03 +0400

    fixed #32

--- plugins/locationselector/inc/locationselector.country.php
+++ plugins/locationselector/inc/locationselector.country.php
@@ -12,26 +12,46 @@
 
 defined('COT_CODE') or die('Wrong URL');
 
+if($a == 'update')
+{
+	$countriesfilter = cot_import('enabled_countries', 'P', 'ARR');
+	
+	$rconfig['config_value'] = implode(',', $countriesfilter);
+	$db->update($db_config, $rconfig, "config_cat='locationselector' AND config_name='countriesfilter'");
+	$cache && $cache->clear();
+	
+	cot_redirect(cot_url('admin', 'm=other&p=locationselector', '', true));
+}
+
 $t = new XTemplate(cot_tplfile('locationselector.country', 'plug'));
 
-$jj = 0;
+$countriesfilter = str_replace(' ', '', $cfg['plugin']['locationselector']['countriesfilter']);
+$countriesfilter = explode(',', $countriesfilter);
 
+$jj = 0;
 foreach ($cot_countries as $code => $name)
 {
 	$jj++;
 	
 	$flag = (!file_exists('images/flags/'.$code.'.png')) ? '00' : $code;
+	
 	$t->assign(array(
 		"COUNTRY_ROW_CODE" => $code,
 		"COUNTRY_ROW_NAME" => $name,
 		"COUNTRY_ROW_URL" => cot_url('admin', 'm=other&p=locationselector&n=region&country=' . $code),
 		"COUNTRY_ROW_FLAG" => cot_rc('icon_flag', array('code' => $flag, 'alt' => '')),
 		"COUNTRY_ROW_NUM" => $jj,
-		"COUNTRY_ROW_ODDEVEN" => cot_build_oddeven($jj)
+		"COUNTRY_ROW_ODDEVEN" => cot_build_oddeven($jj),
+		"COUNTRY_ROW_CHECKED" => (in_array($code, $countriesfilter)) ? true : false,
 	));
 
 	$t->parse("MAIN.ROWS");
 }
+
+$t->assign(array(
+	'LOCATIONSELECTOR_FORM_UPDATE' => cot_url('admin', 'm=other&p=locationselector&a=update'),
+));
+
 if($jj == 0)
 {
 	$t->parse("MAIN.NOROWS");
--- plugins/locationselector/tpl/locationselector.country.tpl
+++ plugins/locationselector/tpl/locationselector.country.tpl
@@ -1,12 +1,25 @@
 <!-- BEGIN: MAIN -->
 <div class="block">
 	<h3>{PHP.L.ls_countries}</h3>
-	<!-- BEGIN: ROWS -->
-	<a href="{COUNTRY_ROW_URL}" class="thumbicons">
-		{COUNTRY_ROW_FLAG}
-		{COUNTRY_ROW_NAME}
-	</a>
-	<!-- END: ROWS -->
+	<form action="{LOCATIONSELECTOR_FORM_UPDATE}" method="post" name="update" id="update" class="ajax">
+		<table class="table">
+		<thead>
+			<tr>
+				<th class="width10">
+					<!-- IF {PHP.cfg.jquery} --><input class="checkbox" type="checkbox" value="{PHP.L.Yes}/{PHP.L.No}" onclick="$('.checkbox').attr('checked', this.checked);" /><!-- ENDIF -->
+				</th>
+				<th>{PHP.L.Country}</th>
+			</tr>	
+		</thead>
+		<!-- BEGIN: ROWS -->
+		<tr>
+			<td class="width10"><input type="checkbox" class="checkbox" name="enabled_countries[]" <!-- IF {COUNTRY_ROW_CHECKED} -->checked="checked"<!-- ENDIF --> value="{COUNTRY_ROW_CODE}"/></td>
+			<td>{COUNTRY_ROW_FLAG} <a href="{COUNTRY_ROW_URL}" class="thumbicons">{COUNTRY_ROW_NAME}</a></td>
+		</tr>
+		<!-- END: ROWS -->
+		</table>
+		<button type="submit" class="btn btn-success">{PHP.L.Update}</button>
+	</form>
 	<!-- BEGIN: NOROWS -->
 	<p>{PHP.L.ls_nocountries}</p>
 	<!-- END: NOROWS -->
