9a77068a03dd66c8588424ea7ca796587ac7d061
devkont <yusupovb@gmail.com>
2014-02-06 13:33:59 +0400

    fixed #37

--- plugins/locationselector/lang/locationselector.en.lang.php
+++ plugins/locationselector/lang/locationselector.en.lang.php
@@ -14,23 +14,23 @@ defined('COT_CODE') or die('Wrong URL.');
 
 // Plugin configuration
 
-$L['cfg_countriesfilter'] = array('Отображать страны', 'Коды через запятую, пустое значение - без фильтра стран');
+$L['cfg_countriesfilter'] = array('Show country', 'codes separated by commas, blank - no filter countries');
 
-$L['ls_addcountry'] = 'Добавить страну';
-$L['ls_countries'] = 'Страны';
-$L['ls_nocountries'] = 'Список стран пуст';
+$L['ls_addcountry'] = 'Add country';
+$L['ls_countries'] = 'Countries';
+$L['ls_nocountries'] = 'The list of countries is empty';
 
-$L['ls_addregion'] = 'Добавить регион';
-$L['ls_regions'] = 'Регионы';
-$L['ls_noregions'] = 'Список регионов пуст';
+$L['ls_addregion'] = 'Add region';
+$L['ls_regions'] = 'Regions';
+$L['ls_noregions'] = 'The list of regions is empty';
 
-$L['ls_addcity'] = 'Добавить город';
-$L['ls_cities'] = 'Города';
-$L['ls_nocities'] = 'Список городов пуст';
+$L['ls_addcity'] = 'Add city';
+$L['ls_cities'] = 'City';
+$L['ls_nocities'] = 'The list of cities is empty';
 
-$L['ls_newcity_newstr'] = 'каждый город в отдельной строке';
-$L['ls_newcity_list'] = 'Укажите список городов';
+$L['ls_newcity_newstr'] = 'each city on a separate line';
+$L['ls_newcity_list'] = 'Specify a list of cities';
 
-$L['select_country'] = 'Выберите страну';
-$L['select_region'] = 'Выберите регион';
-$L['select_city'] = 'Выберите город';
+$L['select_country'] = 'Select a country';
+$L['select_region'] = 'Select a region';
+$L['select_city'] = 'Select a city';
