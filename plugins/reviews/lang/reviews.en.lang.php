74788fceb644b64b0d3c10b61f383741d21957be
devkont <yusupovb@gmail.com>
2014-04-25 10:00:01 +0400

    fixed #70

--- plugins/reviews/lang/reviews.en.lang.php
+++ plugins/reviews/lang/reviews.en.lang.php
@@ -14,20 +14,20 @@ defined('COT_CODE') or die('Wrong URL.');
 /**
  * Module Config
  */
-$L['cfg_checkprojects'] = 'Разрешить добавлять отзывы только при наличии совместных проектов';
+$L['cfg_checkprojects'] = 'Allow add reviews only at presence of joint projects';
 $L['cfg_userall'] = 'Show on user details page all reviews';
 
 $L['review_chooseprj'] = 'Choose project';
 $L['review_reviewforproject'] = 'For';
 $L['reviews_projectsonly'] = 'Reviews can be left only for the projects for which you worked.';
-$L['review_text'] = 'Текст отзыва';
-$L['review_score'] = 'Оценка';
-$L['review_review'] = 'Отзыв';
-$L['review_reviews'] = 'Отзывы';
-$L['review_empty_text'] = 'Отзыв не может быть пустым';
-$L['review_empty_score'] = 'Укажите оценку';
-$L['review_add_review'] = 'Добавить отзыв';
-$L['review_edit_review'] = 'Редактировать отзыв';
+$L['review_text'] = 'Text of review';
+$L['review_score'] = 'Score';
+$L['review_review'] = 'Review';
+$L['review_reviews'] = 'Reviews';
+$L['review_empty_text'] = 'Review can not be empty';
+$L['review_empty_score'] = 'Specify evaluation';
+$L['review_add_review'] = 'Add review';
+$L['review_edit_review'] = 'Edit eview';
 
 $L['review_score_values'] = array(1, -1);
-$L['review_score_titles'] = array('Положительный', 'Негативный');
+$L['review_score_titles'] = array('Positive', 'Negative');
