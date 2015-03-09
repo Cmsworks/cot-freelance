19ce571247e05c1d3e1e1163046e23878885e053
devkont <yusupovb@gmail.com>
2015-03-09 12:45:29 +0300

    fixed #128

--- plugins/usercategories/setup/usercategories.uninstall.sql
+++ plugins/usercategories/setup/usercategories.uninstall.sql
@@ -2,5 +2,5 @@
  * Completely removes usercategories data
  */
 
-DELETE FROM `cot_structure` WHERE structure_area = 'users';
-DELETE FROM `cot_auth` WHERE auth_code = 'users' AND auth_option != 'a';
+DELETE FROM `cot_structure` WHERE structure_area = 'usercategories';
+DELETE FROM `cot_auth` WHERE auth_code = 'usercategories';
