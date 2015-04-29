0cd058db87a7827a8028496dbf885ebe07b5c7af
devkont <yusupovb@gmail.com>
2015-04-29 19:42:50 +0300

    fixed #171

--- plugins/mavatars/mavatars.list.loop.php
+++ plugins/mavatars/mavatars.list.loop.php
@@ -15,26 +15,3 @@ Hooks=page.list.loop
 
 defined('COT_CODE') or die('Wrong URL');
 
-
-	require_once cot_incfile('mavatars', 'plug');
-	global $mav_rowset_list;
-	global $db_mavatars, $db;
-
-	if (!isset($mav_rowset_list))
-	{
-		// Load tags for all entries with 1 query
-		$rowset_copy = $sqllist_rowset;
-		reset($rowset_copy);
-		$mav_items = array();
-		foreach ($rowset_copy as $t_row)
-		{
-			$mav_items[] = $t_row['page_id'];
-		}
-		unset($rowset_copy);
-		$mav_code = implode("','", $mav_items);
-		$mav_rowset_list = $db->query("SELECT * FROM $db_mavatars WHERE mav_extension ='page' AND
-				 mav_code IN ('".$mav_code."') ORDER BY mav_code ASC, mav_order ASC, mav_item ASC")->fetchAll();
-	
-	}
-
-
