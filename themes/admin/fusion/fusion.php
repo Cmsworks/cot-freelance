682ac1bad435eb7ff417f4f88de96317fd602742
devkont <yusupovb@gmail.com>
2015-09-18 22:14:40 +0400

    fixed #208

--- themes/admin/fusion/fusion.php
+++ themes/admin/fusion/fusion.php
@@ -5,7 +5,7 @@ defined('COT_CODE') or die('Wrong URL');
 $R['code_title_page_num'] = ' (' . $L['Page'] . ' {$num})';
 $R['link_pagenav_current'] = '<li class="active"><a href="{$url}"{$event}{$rel}>{$num}</a></li>';
 $R['link_pagenav_first'] = '<li><a href="{$url}"{$event}{$rel}>'.$L['pagenav_first'].'</a></li>';
-$R['link_pagenav_gap'] = '<li>...</li>';
+$R['link_pagenav_gap'] = '<li class="disabled"><a href="javascript:void();">...</a></li>';
 $R['link_pagenav_last'] = '<li><a href="{$url}"{$event}{$rel}>'.$L['pagenav_last'].'</a></li>';
 $R['link_pagenav_main'] = '<li><a href="{$url}"{$event}{$rel}>{$num}</a></li>';
 $R['link_pagenav_next'] = '<li><a href="{$url}"{$event}{$rel}>'.$L['pagenav_next'].'</a></li>';
