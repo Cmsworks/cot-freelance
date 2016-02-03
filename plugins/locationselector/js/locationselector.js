54af710d605f7053ecde6f4702193a491013e25c
cotemplate <cotemplate@mail.ru>
2016-02-04 00:01:58 +0400

    Перевод на json

--- plugins/locationselector/js/locationselector.js
+++ plugins/locationselector/js/locationselector.js
@@ -1,51 +1,95 @@
+$(document).on("change", "#locselectcountry", function(){
+    var x = $('input[name=x]').val();
+    var country = $(this).val();
+    var region_id = 'locselectregion';
+    var city_id = 'locselectcity';
+    var Parent = $('#'+region_id).parent();
 
-$('#locselectcountry').live("change", function(){
-	var parent = $(this).closest('.locselect');
-	var region = $(parent).find('#locselectregion');
-	var region_name = $(region).attr('name');
-	var city = $(parent).find('#locselectcity');
-	var val = $(this).val();
-	
-	var cityoptionfirst = $(city).find('option:first').text();
-	var regionoptionfirst = $(region).find('option:first').text();
-	if(val != '0')
-	{
-		$.get('index.php?r=locationselector', {country: val},
-		function(data){
-			$(region).replaceWith(data).attr('name');
-			$(parent).find('#locselectregion').attr('name', region_name);
-			$(city).html('<option value="0">' + cityoptionfirst + '</option>');
-			$(city).attr('disabled', 'disabled');
-		});
-	}
-	else
-	{
-		$(region).html('<option value="0">' + regionoptionfirst + '</option>');
-		$(region).attr('disabled', 'disabled');
-		$(city).html('<option value="0">' + cityoptionfirst + '</option>');
-		$(city).attr('disabled', 'disabled');
-	}
-	
+    var regionOptionFirst = $('#'+ region_id).find('option:first').text();
+    var cityOptionFirst = $('#'+city_id).find('option:first').text();
+
+    $('#'+ region_id).html('<option value="0">' + regionOptionFirst + '</option>');
+    $('#'+city_id).html('<option value="0">' + cityOptionFirst + '</option>');
+
+    $('#'+region_id).attr('disabled', 'disabled');
+    $('#'+city_id).attr('disabled', 'disabled');
+
+    $('#'+region_id+'_name').val('');
+    $('#'+city_id+'_name').val('');
+
+    $('#loading').remove();
+
+    if (country != '0'){
+        var loader_bg = $('<span>', {
+            id: "loading",
+            class: "loading"
+        }).css('position', 'absolute').css('opacity', 0);
+        loader_bg.html('<img src="./images/spinner.gif" alt="loading" alt="Loading...">');
+		Parent.append(loader_bg).css('opacity', 0.5);
+        $('#'+region_id).html('<option> --- </option>');
+
+        $.post('index.php?r=locationselector', { country: country, x: x }, function(data) {
+            var opts = '';
+            $.each(data.regions, function(index, value) {
+               opts = opts + '<option value="'+index+'">'+value+'</option>';
+            });
+            $('#'+region_id).html(opts);
+            if (data.disabled == 0){
+                $('#'+region_id).attr('disabled', null);
+            }
+            loader_bg.remove();
+			Parent.css('opacity', 1);
+        }, "json");
+    }else{
+
+    }
 });
 
-$('#locselectregion').live("change", function(){
-	var parent = $(this).closest('.locselect');
-	var city = $(parent).find('#locselectcity');
-	var city_name = $(city).attr('name');
-	var val = $(this).val();
-	
-	if(val != '0')
-	{	
-		$.get('index.php?r=locationselector', {region: val},
-		function(data){
-			$(city).replaceWith(data).attr('name', city_name);
-			$(parent).find('#locselectcity').attr('name', city_name);
-		});
-	}
-	else
-	{
-		var cityoptionfirst = $(city).find('option:first').text();
-		$(city).html('<option value="0">' + cityoptionfirst + '</option>');
-		$(city).attr('disabled', 'disabled');
-	}
+$(document).on("change", "#locselectregion", function(){
+    var id = $(this).attr('id');
+    var x = $('input[name=x]').val();
+    var region = $(this).val();
+    var city_id = 'locselectcity';
+
+    var Parent = $('#'+city_id).parent();
+
+    var cityOptionFirst = $('#'+city_id).find('option:first').text();
+    $('#'+city_id).html('<option value="0">' + cityOptionFirst + '</option>');
+
+    $('#'+ id +'_name').val($('#'+ id + ' option:selected').text());
+    $('#'+city_id+'_name').val('');
+
+    $('#loading').remove();
+
+    $('#'+city_id).attr('disabled', 'disabled');
+
+    if (region != '0'){
+        var loader_bg = $('<span>', {
+            id: "loading",
+            class: "loading"
+        }).css('position', 'absolute').css('opacity', 0);
+        loader_bg.html('<img src="./images/spinner.gif" alt="loading" alt="Loading...">');
+        Parent.append(loader_bg).css('opacity', 0.5);
+        $('#'+city_id).html('<option> --- </option>');
+
+        $.post('index.php?r=locationselector', { region: region, x: x }, function(data) {
+            var opts = '';
+            $.each(data.cities, function(index, value) {
+                opts = opts + '<option value="'+index+'">'+value+'</option>';
+            });
+            $('#'+city_id).html(opts);
+            if (data.disabled == 0){
+                $('#'+city_id).attr('disabled', null);
+            }
+            loader_bg.remove();
+			Parent.css('opacity', 1);
+        }, "json");
+    }else{
+
+    }
+});
+
+$(document).on("change", "#locselectcity", function(){
+    var id = $(this).attr('id');
+    $('#'+ id +'_name').val($('#'+ id + ' option:selected').text());
 });
