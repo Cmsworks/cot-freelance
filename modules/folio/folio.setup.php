4e387ab0f74890b2176cdb2800f9853142633602
devkont <yusupovb@gmail.com>
2016-02-19 15:06:39 +0300

    fixed #259

--- modules/folio/folio.setup.php
+++ modules/folio/folio.setup.php
@@ -4,7 +4,7 @@
  * Code=folio
  * Name=Folio
  * Description=Портфолио
- * Version=2.5.12
+ * Version=2.5.13
  * Date=24.11.2012
  * Author=CMSWorks Team
  * Copyright=Copyright (c) CMSWorks.ru, littledev.ru
@@ -47,7 +47,7 @@
  * folio module
  *
  * @package folio
- * @version 2.5.12
+ * @version 2.5.13
  * @author CMSWorks Team
  * @copyright Copyright (c) CMSWorks.ru, littledev.ru
  * @license BSD
