a1cd4750dd118ddd2ac8dc573c2aa9ebdce356bb
Булат Юсупов <yusupovb@gmail.com>
2017-02-28 09:10:27 +0300

    Fixed #307

--- modules/payments/payments.setup.php
+++ modules/payments/payments.setup.php
@@ -4,7 +4,7 @@
  * Code=payments
  * Name=Payments
  * Description=Платежный модуль
- * Version=2.0
+ * Version=2.0.1
  * Date=
  * Author=CMSWorks Team
  * Copyright=Copyright (c) CMSWorks.ru
@@ -36,7 +36,7 @@
  * Payments module
  *
  * @package payments
- * @version 2.0
+ * @version 2.0.1
  * @author CMSWorks Team
  * @copyright Copyright (c) CMSWorks.ru
  * @license BSD
