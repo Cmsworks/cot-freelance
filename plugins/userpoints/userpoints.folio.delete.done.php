af04dab05b0b614918ef5efc3a5ca0c185f47979
Булат Юсупов <yusupovb@gmail.com>
2017-04-27 15:57:53 +0300

    #319
    Пересчет баллов пользователя

--- plugins/userpoints/userpoints.folio.delete.done.php
+++ plugins/userpoints/userpoints.folio.delete.done.php
@@ -16,6 +16,9 @@
  */
 defined('COT_CODE') or die('Wrong URL.');
 
-global $db_userpoints;
+global $db_userpoints, $db_users;
 
 $db->delete($db_userpoints, "item_type IN ('portfolioaddtocat', 'portfoliodeltocat') AND item_itemid=".$id);
+
+$uuserpoints = $db->query("SELECT SUM(item_point) as summ FROM $db_userpoints WHERE item_userid=" . (int)$ritem['item_userid'])->fetchColumn();
+$db->update($db_users, array('user_userpoints' => $uuserpoints), "user_id=" . (int)$ritem['item_userid']);
